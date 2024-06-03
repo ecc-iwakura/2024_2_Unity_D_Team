37479fc944379ede34989717b2cf47fdb15110ba
MAETEL\2237026 <2237026@ecc.ac.jp>
2024-06-03 16:28:50 +0900

    no message

---
+++ Assets/script/Star_Inventry_button.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Star_Inventry_button : MonoBehaviour
+{
+    private Star_bag manager;
+
+    // ボタンを配列として定義
+    [SerializeField] private Button[] _starButton;
+    public GameObject[] star_prefabs;
+
+    public Transform spawnPoint;    // EmptyオブジェクトのTransform
+    private void Start()
+    {
+        manager = GetComponent<Star_bag>();
+    }
+    public void StarSelect(int stars)
+    {
+        int i = manager.GetScore(stars);
+        if (i > 0)
+        {
+            Instantiate(star_prefabs[stars], spawnPoint.position, spawnPoint.rotation);
+            manager.AddScore(stars,-1);
+        }
+       
+    }
+}
