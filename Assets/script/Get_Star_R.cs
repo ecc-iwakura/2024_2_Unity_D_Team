927d9128988e709815ef5993da73f521d70f5bc8
MAETEL\2237026 <2237026@ecc.ac.jp>
2024-06-04 16:18:49 +0900

    no message

---
+++ Assets/script/Get_Star_R.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Get_Star_R : MonoBehaviour
+{
+    public string[] targetTags = { "Star0", "Star1", "Star2", "Star3" };
+
+    private Star_bag manager;
+    private void Start()
+    {
+        manager = GetComponent<Star_bag>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+            if (hit.collider != null) // Rayが何かに当たったかチェック
+            {
+                for (int i = 0; i < targetTags.Length; i++)
+                {
+                    if (hit.transform.gameObject.tag == targetTags[i]) // タグを比較
+                    {
+                        Debug.Log("Clicked on object with tag: " + targetTags[i]);
+                        manager.AddScore(i, 5);
+                        Destroy(hit.collider.gameObject);
+                        break; // タグが一致したらループを抜ける
+                    }
+                }
+            }
+        }
+    }
+}
