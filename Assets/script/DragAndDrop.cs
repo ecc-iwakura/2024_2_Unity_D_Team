9cb6710d28edca3883a40e57c7afe4a79271d1e0
MAETEL\2237026 <2237026@ecc.ac.jp>
2024-07-08 14:18:29 +0900

    no message

--- Assets/script/DragAndDrop.cs
+++ Assets/script/DragAndDrop.cs
@@ -7,13 +7,9 @@ public class DragAndDrop : MonoBehaviour
     private bool isDragging = false;
     private Vector3 offset;
 
+
     void OnMouseDown()
     {
-        // スプライトのレンダラーのsortingLayerNameを設定して、Stellaレイヤーを優先させる
-        GetComponent<SpriteRenderer>().sortingLayerName = "Stella";
-        GetComponent<SpriteRenderer>().sortingOrder = 0; // 必要に応じてsortingOrderも設定
-
-        // オブジェクトの位置とマウス位置の差分を計算する
         offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
         isDragging = true;
     }
@@ -22,13 +18,11 @@ public class DragAndDrop : MonoBehaviour
     {
         if (isDragging)
         {
-            // マウス位置と差分から新しい位置を計算する
             Vector3 newPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0)) + offset;
             transform.position = newPosition;
 
-            // マウスのスクロールホイールの入力に基づいてオブジェクトを回転させる
             float scrollDelta = Input.GetAxis("Mouse ScrollWheel");
-            transform.Rotate(Vector3.forward, scrollDelta * 100f); // 回転速度を調整する場合は、100fを変更してください
+            transform.Rotate(Vector3.forward, scrollDelta * 100f); // 角度の変更速度を調整するために100fを変更することができます
         }
     }
 
