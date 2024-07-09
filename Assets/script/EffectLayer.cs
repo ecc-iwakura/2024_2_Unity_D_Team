0756e718b618466b59806e127786041fc235d73f
MAETEL\2237026 <2237026@ecc.ac.jp>
2024-07-09 14:45:18 +0900

    no message

---
+++ Assets/script/EffectLayer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectLayer : MonoBehaviour
+{
+    private string EFFECT_SORTING_LAYER_NAME = "Effect";
+    private Renderer objectRenderer;
+
+    private void Awake()
+    {
+        objectRenderer = GetComponent<Renderer>();
+        if (objectRenderer != null)
+        {
+            objectRenderer.sortingLayerName = EFFECT_SORTING_LAYER_NAME;
+        }
+
+        SetSortingLayer(transform);
+    }
+
+    private void SetSortingLayer(Transform parent)
+    {
+        foreach (Transform child in parent)
+        {
+            Renderer childRenderer = child.GetComponent<Renderer>();
+            if (childRenderer != null)
+            {
+                childRenderer.sortingLayerName = EFFECT_SORTING_LAYER_NAME;
+            }
+
+            // 再帰的に子オブジェクトに対して同じ処理を行う
+            SetSortingLayer(child);
+        }
+    }
+}
--- Assets/script/MetorCreat.cs
+++ Assets/script/MetorCreat.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MetorCreat : MonoBehaviour
 {
-    [SerializeField]
-    private Camera_Control manager;//Star_bagが入ったオブジェクトをアタッチ
+    //[SerializeField]
+    //private Camera_Control manager;//Star_bagが入ったオブジェクトをアタッチ
     
 
     public GameObject  star_prefabs;
     public int y = 0;
     public int x = 0;
+    [SerializeField] GameObject smokeEffect;
 
-   
 
     public void Spawnmeteor()
     {
@@ -26,7 +27,9 @@ public class MetorCreat : MonoBehaviour
         if (other.gameObject.tag == "Meteor")
         {
             Destroy(other.gameObject);
-            manager.gameover();
+            //manager.gameover();
+            GameObject smoke = Instantiate(smokeEffect, transform.position, Quaternion.identity);
+            Destroy(smoke, 30);
         }
     }
 }
